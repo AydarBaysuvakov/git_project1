2e773984cef3059b1988d59720470bc41e790189
aydar <aydar.baysuvakov@gmail.com>
2022-12-15 20:10:13 +0500

    name

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+# Aydar
+def my_superfunction():
+    print('What an awesome print!')
+
 def main():
     print('My first git program')
     print('This is first change')
     print('Second change')
     print('continue')
+
 
 
 if __name__ == '__main__':
